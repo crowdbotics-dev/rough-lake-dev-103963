26b925de01b306a751a0fe67bdc56bee06e76e88
Crowdbotics <engineering@crowdbotics.com>
2023-07-21 23:27:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "rough-lake-dev-103963",
+  url: "https://rough-lake-dev-103963.botics.co",
+  api: "https://rough-lake-dev-103963.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
